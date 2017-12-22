c7f43177ee5e9d6a131b200be85b92d40892e4fb
xiedacon <xiedacon@qq.com>
2017-12-23 00:56:23 +0800

    2017-12-23

--- settings/~/deploy.js
+++ settings/~/deploy.js
@@ -3,15 +3,15 @@
 const fs = require('fs')
 const { resolve } = require('path')
 
-symlinkAll(__dirname, resolve(process.env.HOME, '.config'))
+symlinkAll(__dirname, resolve(process.env.HOME))
 
-function symlinkAll(rootPath, linkRootPath) {
+function symlinkAll (rootPath, linkRootPath) {
   let files = fs.readdirSync(rootPath)
 
   files.forEach((file) => {
     let path = resolve(rootPath, file)
     if (path === __filename) return
-    
+
     let linkPath = resolve(linkRootPath, file)
     if (fs.existsSync(linkPath)) {
       if (fs.statSync(path).isDirectory()) return symlinkAll(path, linkPath)
